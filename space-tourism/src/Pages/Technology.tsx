2d6e1cef215feff3fea182381a50aa025e257fc5
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2024-06-18 12:48:22 +0100

    Updated the css files 🚀🚀

--- space-tourism/src/Pages/Technology.tsx
+++ space-tourism/src/Pages/Technology.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Technology.css";
 import NavBar from "../Components/NavBar";
 
 function Technology() {
   return (
-    <div>
+    <div className="technology-page">
       <NavBar />
-      <h1>Technology</h1>
+      <div className="crew">
+        <h1 className="pick-destination-h1">
+          <span className="pick-destination-span">03</span> SPACE LAUNCH 101
+        </h1>
+      </div>{" "}
     </div>
   );
 }
