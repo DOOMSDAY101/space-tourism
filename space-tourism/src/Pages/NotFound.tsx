a841fdd3554d7efceeef301beb685494fd43d61c
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2024-06-19 20:12:52 +0100

    404 Page completed 🚀🚀

--- space-tourism/src/Pages/NotFound.tsx
+++ space-tourism/src/Pages/NotFound.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import "../App.css";
+import { Link } from "react-router-dom";
 function NotFound() {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="not-found">
+      <div className="not-found-content">
+        <h1>404</h1>
+        <p>
+          Oops! It seems like you've drifted off course.
+          <br />
+          The cosmic object you were looking for has disappeared beyond the
+          event horizon.
+        </p>
+        <Link to="/">Blast Off Again</Link>
+        <Link to="/destination">Explore The Galaxy</Link>
+      </div>
     </div>
   );
 }
